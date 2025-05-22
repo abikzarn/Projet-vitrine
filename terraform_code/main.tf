481ecc114308d065fa397a96c0170ceb78f4e433
Abdessalam <ab.ikzarn@gmail.com>
2025-05-22 22:06:04 +0200

    changement de ami

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -32,6 +32,15 @@ resource "aws_security_group" "ssh_http_https" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "Next.js dev port"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+}
+
   egress {
     from_port        = 0
     to_port          = 0
